c16ebd318932348bb9f20137b2e9a643ef68d7a4
aYuSh-358 <gauravbagadkar97@gmail.com>
2025-06-26 12:53:01 +0530

    connecting with the live monogdbAlta url

--- src/controllers/sendEmailController.js
+++ src/controllers/sendEmailController.js
@@ -4,9 +4,9 @@ const { promisify } = require("util");
 const readFileAsync = promisify(fs.readFile);
 
 module.exports.sendEmail = async (user) => {
-  const imageAttachment = await readFileAsync(
-    "../../..//Project/Social_Media/uploads/mail/hsmlogo.png"
-  );
+  // const imageAttachment = await readFileAsync(
+  //   "../../..//Project/Social_Media/uploads/mail/hsmlogo.png"
+  // );
   const transporter = nodemailer.createTransport({
     service: "Gmail",
     host: "smtp.gmail.com",
@@ -26,47 +26,21 @@ module.exports.sendEmail = async (user) => {
       text: `Hellow ${user.userName}`,
       html: `<h1>Hello ${user.userName},</h1>
         <h3>You registered successfully</h3>
-    <img src="cid:hsmlogo" alt="Embedded Image">`,
-      attachments: [
-        {
-          filename: "image.png",
-          content: imageAttachment,
-          encoding: "base64",
-          cid: "hsmlogo",
-        },
-        {
-          filename: "hsmlogo.png",
-          path: __dirname + "../../../uploads/mail/hsmlogo.png",
-          cid: "uniq-hsmlogo.png",
-        },
-      ],
+    // <img src="cid:hsmlogo" alt="Embedded Image">`,
+      //   attachments: [
+      //     {
+      //       filename: "image.png",
+      //       content: imageAttachment,
+      //       encoding: "base64",
+      //       cid: "hsmlogo",
+      //     },
+      //     {
+      //       filename: "hsmlogo.png",
+      //       path: __dirname + "../../../uploads/mail/hsmlogo.png",
+      //       cid: "uniq-hsmlogo.png",
+      //     },
+      //   ],
     });
     // console.log("Message sent:", info.messageId,);
   })();
 };
-
-// module.exports.sendEmail = async () => {
-//   // Create a test account or replace with real credentials.
-//   const transporter = nodemailer.createTransport({
-//     host: "smtp.ethereal.email",
-//     port: 587,
-//     secure: false, // true for 465, false for other ports
-//     auth: {
-//       user: "chyna85@ethereal.email",
-//       pass: "MuH58mfKBneGBG41Wt",
-//     },
-//   });
-
-//   // Wrap in an async IIFE so we can use await.
-//   (async () => {
-//     const info = await transporter.sendMail({
-//       from: '"Ayush" <chyna85@ethereal.email>',
-//       to: "ayush.dongre.btechcyb@ghrietn.raisoni.net",
-//       subject: "Hello Ayush",
-//       text: "Hello Ayush", // plain‑text body
-//       html: "<b>Hello Ayush</b>", // HTML body
-//     });
-
-//     console.log("Message sent:", info.messageId);
-//   })();
-// };
--- src/middleware/multerConfig.js
+++ src/middleware/multerConfig.js
@@ -1,23 +1,3 @@
-// const multer = require("multer");
-// const path = require("path");
-
-// const uploadDir = path.join(__dirname, "../../uploads/DP");
-
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, uploadDir);
-//   },
-//   filename: (req, file, cb) => {
-//     //const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-//     const uniqueSuffix = file.originalname;
-//     const ext = path.extname(file.originalname);
-//     const newFileName = `post-${uniqueSuffix}${ext}`;
-//     cb(null, newFileName);
-//   },
-// });
-
-// module.exports.upload = multer({ storage });
-
 const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
@@ -46,10 +26,9 @@ const storage = multer.diskStorage({
     cb(null, uploadDir);
   },
   filename: (req, file, cb) => {
-    //const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    const uniqueSuffix = file.originalname;
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
     const ext = path.extname(file.originalname);
-    const newFileName = `post-${uniqueSuffix}`;
+    const newFileName = `${uniqueSuffix}${ext}`;
     cb(null, newFileName);
   },
 });
