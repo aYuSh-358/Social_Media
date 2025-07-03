cc420efde33dae2dd0a6360d409e4ba9a22f7d71
Ayushiz02 <zodapeayushi@gmail.com>
2025-07-03 19:33:37 +0530

    Created memberList, leaveGroup, removeMember in groupChatController

--- src/routes/groupChatRoutes.js
+++ src/routes/groupChatRoutes.js
@@ -7,6 +7,9 @@ const authMiddleware = require("../middleware/authMiddleware")
 router.post("/createGroup", authMiddleware, controller.createGroup);
 router.post("/addMember", authMiddleware, controller.addMember);
 router.post("/makeAdmin", authMiddleware, controller.makeAdmin);
+router.get("/memberList/:id", controller.memberList);
+router.post("/leaveGroup", authMiddleware, controller.leaveGroup);
+router.post("/removeMember", authMiddleware, controller.removeMember);
 
 
 module.exports = router;
